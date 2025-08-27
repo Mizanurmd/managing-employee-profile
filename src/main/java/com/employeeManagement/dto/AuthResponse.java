19a15ec502b6a4031bc44095015bfda5f184d523
Mizanurmd <mizanur021991@gmail.com>
2025-08-27 21:58:18 +0600

    Added loginRequest & authResponse

---
+++ src/main/java/com/employeeManagement/dto/AuthResponse.java
@@ -0,0 +1,19 @@
+package com.employeeManagement.dto;
+
+import com.employeeManagement.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AuthResponse {
+    private int statusCode;
+    private String message;
+    private String token;
+    private String refreshToken;
+    private String expirationTime;
+    private Role role;
+    private String error;
+}
